2ae77809fcc35fa87aeafdc6ba67aff9ad158705
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-14 10:09:06 +0100

    correzione all'interno dello script

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 // individuo le variabili presenti
-const button = document.getElementById("button");
+const buttonElement = document.querySelector('button');
+const numberKm = document.getElementById('area-km');
+const userAge = document.getElementById('area-age');
+const costperKm = 0.1976;
 
-button.addEventListener("click", function() {
+buttonElement.addEventListener( 'click', function(){
 
     //Assegno ad una variabile i valori inseriti dall'utente
+    const distance = parseFloat(numberKm.value)
+    const age = parseInt(userAge.value)
+    const outputElement = document.getElementById('total-price');
 
-    const numberKm = document.getElementById("area-km").value;
-    const userAge = document.getElementById("area-age").value;
-    const outputElement = document.getElementById('total-price')
-
-    if (!numberKm) {
-        alert("Distanza non valida!");
+    if(!numberKm) {
+        alert("Inserire di nuovo la distanza!");
         return;
     }
 
     if(!userAge) {
-        alert("Inserisci una fascia d'eta' per continuare");
+        alert("Inserisci la tua età!");
         return;
     }
 
     // Calcolo del prezzo totale
-    let ticketCost = numberKm * 0.1976;
+    let ticketCost = numberKm * costperKm;
 
 
     // Calcolo del prezzo scontato in base all'età
     if (userAge < 18 ) {
-        console.log("teen")
         ticketCost = ticketCost * 17.65 / 100;
     } else if (userAge >= 65 ) {
-        console.log("elder")
         ticketCost = ticketCost * 53.27 / 100;
     }
 
-    outputElement.innerHTML = ticketCost
-
+    console.warn(distance, age, ticketCost);
 
 })
